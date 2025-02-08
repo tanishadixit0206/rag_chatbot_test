83659fa19b6edc03039b67fcc1edfb34ac759125
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-02-08 18:48:24 +0530

    feat: Migrates to Google Flan Model for improved performance.

--- src/models/model_loader.py
+++ src/models/model_loader.py
@@ -1,7 +1,7 @@
-from sentence_transformers import SentenceTransformer
-from transformers import pipeline, BartForConditionalGeneration, BartTokenizer
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-def load_summarizer(model_name):
-    model = BartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = BartTokenizer.from_pretrained(model_name)
+def load_summarizer():
+    model_name = "google/flan-t5-large"
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
     return model, tokenizer
--- src/models/summarizer.py
+++ src/models/summarizer.py
@@ -1,13 +1,29 @@
-from transformers import BartForConditionalGeneration, BartTokenizer
 from .model_loader import load_summarizer
+from language_tool_python import LanguageTool
 
-model_name = "facebook/bart-large-cnn"
-model = BartForConditionalGeneration.from_pretrained(model_name)
-tokenizer = BartTokenizer.from_pretrained(model_name)
+model_name = "google/flan-t5-xl"
+
 
 def generate_result(prompt): 
-    model, tokenizer = load_summarizer(model_name)
+    model, tokenizer = load_summarizer()
     inputs = tokenizer(prompt, return_tensors="pt", truncation=True, padding="longest")
-    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=500, min_length=25)   
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary.replace("  ","")
+    inputs = tokenizer(prompt, return_tensors="pt", truncation=True, padding=True)
+    outputs = model.generate(
+    inputs['input_ids'], 
+    max_length=300, 
+    min_length=50, 
+    num_beams=5, 
+    temperature=0.7,  
+    top_k=50,
+    top_p=0.9,
+    early_stopping=True,
+    do_sample=True
+)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    if not response.endswith(('.', '!', '?')):
+        response += '.'
+    response = response[0].capitalize() + response[1:]
+    
+    tool = LanguageTool('en-US')
+    corrected_response = tool.correct(response)
+    return(corrected_response)
